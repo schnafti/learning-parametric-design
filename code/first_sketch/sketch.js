c99e3ee44c121db079b9fd8b6f29c83d91850e90
schnafti <117223407+schnafti@users.noreply.github.com>
2022-11-02 12:27:37 +0100

    task-01

---
+++ code/first_sketch/sketch.js
@@ -0,0 +1,13 @@
+function preload(){
+  // preload assets
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0, 100, 200);
+}
+triangle(30,30,30,30,30,30);
+describe('white triangle with black outline in mid-rifht of canvas');
---
+++ code/task-01/sketch.js
@@ -0,0 +1,13 @@
+function preload(){
+  // preload assets
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(100);
+  triangle (50, 100, 150, 10, 10, 10)
+  frame (200)
+}
